2d14f7a5fbf10b4d9be4f6cefaf2ccc5274ec5a8
euglenach <midorimushiiii@gmail.com>
2018-09-28 21:45:52 +0900

    3ごと表示

--- 4S/3times.java
+++ 4S/3times.java
@@ -1,1 +1,14 @@
- 
+ import java.util.*;
+import java.util.stream.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String args[ ]) {
+        
+        IntStream.range(1, 101)
+                 .filter(n -> n % 3 == 0)
+                 .forEach(n -> System.out.println(n));
+    }
+}
+
