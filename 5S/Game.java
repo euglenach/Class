fb736fe4194fba256e2638c7540c2cabc63b2fe7
euglenach <midorimushiiii@gmail.com>
2019-04-17 15:37:27 +0900

    Add files via upload

---
+++ 5S/Game.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.stream.*;
+
+public class Game {
+    public static void main(String[] args) {
+        Cards cards = new Cards(5,"手札");
+        //cards.addCard(1);
+        for (var item: new int[]{1,15,31,49,52}) {
+            cards.addCard(item);
+        }
+        cards.printCards();
+        System.out.println(cards.getSuitNumber(0));
+        cards.printNumbers();
+        cards.printSuitsNumbers();
+        System.out.println(cards.maxNumber());
+        cards.reverse();
+        cards.printCards();
+    }
+}
